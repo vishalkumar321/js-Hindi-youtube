7fc2f9d17253d10ec0b6413031e3a124ba2c7951
vishalkumar321 <vishalkumargupta4546@gmail.com>
2025-06-09 07:26:38 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict";  //treat all js code as newer version
+
+// alert(3 + 3)  // We are using node.js, not browser
+
+console.log(3   // code readability should be high
+    + 3)
+
+console.log("Vishal")
+
+let name = "vishal"
+let age = 18
+let isLoggedIn = false;
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object 
+
+console.log(typeof undefined);  //undefined
+console.log(typeof null);       //object
+console.log(typeof age);        //number
