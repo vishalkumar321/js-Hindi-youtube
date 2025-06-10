2b7efd3165750b3ca30595bf52f24224d783041d
vishalkumar321 <vishalkumargupta4546@gmail.com>
2025-06-10 07:30:06 +0000

    DataType and Memory

--- 01_basics/05_dataType-summary.js
+++ 01_basics/05_dataType-summary.js
@@ -35,4 +35,35 @@ const MyFunction = function(){      // function
 
 console.log(typeof heroes);     // object
 console.log(typeof myObj);      // object
-console.log(typeof MyFunction); //function
+console.log(typeof MyFunction); // function
+console.log(typeof anotherId);  // symbol
+
+// ******************************************** Memory **********************************************************
+
+// Stack(Primitive DataType) || Heap(Non-Primitive DataType)
+
+    // Stack works on pass by value that's why It changes only one value which changes
+
+let myYoutubeName = "hiteshchaudharydotcom"     // string
+
+let anotherName = myYoutubeName
+anotherName = "chaiaurcode"
+
+console.log(myYoutubeName);     // hiteshchaudharydotcom
+console.log(anotherName);       // chaiaurcode
+
+    // Heap works on pass by reference that's why It changes both values 
+
+let userOne = {
+    name : "Vishal",
+    email : "user@google.com",      // object
+    upi : "user@ybl"
+}
+
+let userTwo = userOne
+userTwo.email = "vishal@gmail.com"
+
+console.log(userOne.email);     // vishal@gmail.com
+console.log(userTwo.email);     // vishal@gmail.com
+
+
