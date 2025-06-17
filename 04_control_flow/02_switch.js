571a45bbf986923488436dc37b77ca5581742059
vishalkumar321 <vishalkumargupta4546@gmail.com>
2025-06-17 09:50:36 +0000

    switch

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,54 @@
+// switch statement
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+    const month = 3
+
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("february");
+        break;
+    case 3:
+        console.log("march");                       // march
+        break;
+    case 4:
+        console.log("april");
+        break;
+
+    default:
+        console.log("default month");
+        break;
+}
+
+
+    const month1 = "april"
+
+switch (month1) {
+    case "jan":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("february");
+        break;
+    case "march":                                       // april
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default month");
+        break;
+}
