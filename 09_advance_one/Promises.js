02d7971e8a3e123c28552a671be437c6d5d30e5f
vishalkumar321 <vishalkumargupta4546@gmail.com>
2025-06-24 19:21:53 +0000

    promises

---
+++ 09_advance_one/Promises.js
@@ -0,0 +1,107 @@
+const PromiseOne = new Promise(function(resolve,reject){
+    // Do an Async task
+    //  DB calls, cryptography, network
+    setTimeout(function(){
+        console.log('Async task is complete');
+        resolve()
+    }, 1000);
+})
+
+PromiseOne.then(function(){
+    console.log("promise consumed");
+    
+})
+
+new Promise(function(resolve,reject){
+    setTimeout(function(){
+        console.log("Async 2 task complete");
+        resolve()
+    }, 1000);
+}).then(function(){
+    console.log('Async 2 resolved');
+    
+})
+
+const PromiseThree = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        resolve({userName : 'chai', email : "ChaiAurCode.com"})
+    },1000)
+})
+
+PromiseThree.then(function(user){
+    console.log(user);  
+})
+
+const promiseFour = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        const error = false
+        if(!error){
+            resolve({userName : "vishal", password : "123"})
+        }
+        else{
+            reject("ERROR something went wrong");
+        }
+    },1000)
+})
+
+promiseFour.then( (user) => {
+    console.log(user);
+    return user.userName
+}).then( (username) =>{
+    console.log(username);
+}).catch(function(error){
+    console.log(error);
+}).finally( ()=> {console.log('The promise is either resolved or rejected');
+})
+
+
+
+const promiseFive = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        const error = true
+        if(!error){
+            resolve({userName : "javascript", password : "123"})
+        }
+        else{
+            reject("ERROR js went wrong");
+        }
+    },1000)
+})
+
+async function consumePromiseFive(){
+    try {
+        const response = await promiseFive
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+consumePromiseFive()
+
+// async function getAllUsers(){
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//         const data =await response.json()
+//         console.log(data);
+        
+//     } catch (error) {
+//         console.log(error);  
+//     }
+// }
+
+// getAllUsers()
+
+fetch('https://api.github.com/users/hiteshchoudhary')
+.then( (response =>{
+    return response.json();
+}))
+.then( (data)=>{
+    console.log(data);
+    
+})
+.catch( (error)=>{
+    console.log(error);
+})
+
+// promise.all
